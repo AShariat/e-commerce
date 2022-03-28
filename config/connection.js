276403546c8b3bedf0111e521d9c59dc9e5a21f5
Amir Shariat <amirshariat1987@gmail.com>
2022-03-27 19:19:52 -0600

    get all categories and get one category done

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,6 @@
-// const Sequelize = require("sequelize");
-// require("dotenv").config();
-
-// let sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(
-//       process.env.DB_NAME,
-//       process.env.DB_USER,
-//       process.env.DB_PASSWORD,
-//       {
-//         host: "localhost",
-//         dialect: "mysql",
-//         dialectOptions: {
-//           decimalNumbers: true,
-//         },
-//       }
-//     );
-
-// module.exports = sequelize;
-
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-// create connection to our db
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,14 +4,40 @@ const { Category, Product } = require("../../models");
 // The `/api/categories` endpoint
 
 router.get("/", (req, res) => {
-  res.send("Hello");
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get("/:id", (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res) => {
